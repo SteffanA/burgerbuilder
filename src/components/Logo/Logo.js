741ec2043b8fcd6e5053ff6c50f2e52aaa8ca988
Steffan <sachtmann121@gmail.com>
2020-01-08 00:31:37 -0800

    Setup for altering logo height based on prop passing

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,8 +5,10 @@ import React from 'react'
 import burgerLogo from '../../assets/images/burger-logo.png'
 import classes from './Logo.module.css'
 
-const logo = () => (
-    <div className={classes.Logo}>
+const logo = (props) => (
+    // Pass height as a property to this class to enable responsive design
+    // dependant on which field needs to use the logo
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="MyBurger"/>
     </div>
 )
