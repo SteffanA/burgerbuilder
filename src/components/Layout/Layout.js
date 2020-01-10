e7907e401020a0d99a213e8cf5271617a47bb8c4
Steffan <sachtmann121@gmail.com>
2020-01-10 02:14:59 -0800

    Did sidedrawer toggles/menu btn my own way. Will swap to tut way next

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Auxillary'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer: true,
+    }
 
-export default layout;
+    siderDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerOpenHandler = () => {
+        this.setState({showSideDrawer: true})
+    }
+
+    render() {
+        return (
+         <Aux>
+            <Toolbar btnClick={this.sideDrawerOpenHandler}/>
+            <SideDrawer open={this.state.showSideDrawer} closed={this.siderDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux> 
+        )
+    } 
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,16 @@ import React from 'react'
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import Button from '../../UI/Button/Button'
 
 const toolbar = (props) => ( 
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <Button clicked = {props.btnClick} btnType = {'MENU'}>MENU</Button>
+        {/* Add menu btn, with hamburger icon, let backdrop visible tag thru */}
         <div className={classes.Logo}>
             <Logo height="80%"/>
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
     </header>
